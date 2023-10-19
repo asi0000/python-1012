0bac199216028168319c9a0bb8dcfc6bb15ec7ca
asi0000 <asi0000@nate.com>
2023-10-19 19:56:54 +0900

    20231019 python 1

---
+++ ExamDay03/ex01.py
@@ -0,0 +1,12 @@
+name = input("이름입력 >> ");
+age = int(input("나이입력 >> "));
+gender = input("성별입력 >> ");
+school = input("학교입력 >> ");
+address = input("거지주 입력 >> ");
+
+print("본인이름 : ", name);
+print("본인나이 : ", age);
+print("age변수 type ? ",type(age));
+print("본인성별 : ", gender);
+print("본인학교 : ", school);
+print("거주지 : ", address);
