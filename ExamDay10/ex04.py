71440bbb77ed03ad6f9aee8ac6f5976ec149d0c7
asi0000 <asi0000@nate.com>
2023-11-07 21:24:14 +0900

    20231107 python 2

---
+++ ExamDay10/ex04.py
@@ -0,0 +1,23 @@
+'''
+round() 함수
+- 전달된 인수를 이용해 반올림한 값을 반환
+
+round half even
+짝수에 가까운쪽을 반올림 한다
+'''
+print(round(1.5));
+print(round(1.4));
+print(round(2.225, 2));
+print(round(2.675, 2)); # 컴퓨터의 한계,, 실수를 정확하게 집어내지 못해서 해당문제가 생겨남.
+
+print();
+print(round(2.205, 2));
+print(round(2.215, 2));
+print(round(2.225, 2));
+print(round(2.235, 2));
+print(round(2.245, 2));
+print(round(2.255, 2)); # 반올림하는 앞자리가 홀수 일때 생기는 버그
+
+
+
+
